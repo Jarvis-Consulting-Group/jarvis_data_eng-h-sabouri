7f7d01bb40a347da45a024aae5f96c929e4cbe7f
Hossein Sabouri <hossein_sabouri@hotmail.com>
2023-06-15 20:41:07 +0000

    modifies psql script

--- linux_sql/scripts/psql_docker.sh
+++ linux_sql/scripts/psql_docker.sh
@@ -31,8 +31,8 @@ case $cmd in
   #Create container
 	docker volume create pgdata
   #Start the container
-	container_name="jrvs-psql"
-	docker run --name $container_name -e POSTGRES_PASSWORD=$db_password -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres:9.6-alpine
+	
+	docker run --name $db_username -e POSTGRES_PASSWORD=$db_password -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres:9.6-alpine
   
 	exit $?
 	;;
