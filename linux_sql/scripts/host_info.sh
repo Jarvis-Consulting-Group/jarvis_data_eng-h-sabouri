6295df4a10df1bbc2eb52045be9ab5a37d774e81
Hossein Sabouri <hossein_sabouri@hotmail.com>
2023-06-22 17:11:45 +0000

    host_info modified

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -5,7 +5,7 @@ db_name=$3
 psql_user=$4
 psql_password=$5
 
-if ["$#" -ne 5 ]; then
+if [ "$#" -ne 5 ]; then
   echo "Illegal number of parameters"
   exit 1
 fi
@@ -14,17 +14,16 @@ hostname=$(hostname -f)
 lscpu_out=`lscpu`
 vmstat_mb=`vmstat --unit M`
 
-cpu_number=$(echo "$lscpu_out" | egrep "^CPU\(\s):" | awk '{print $2}' | xargs)
+cpu_number=$(echo "$lscpu_out" | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
 cpu_architecture=$(echo "$lscpu_out"  | egrep "^Architecture:" | awk '{print $2}' | xargs)
 cpu_model=$(echo "$lscpu_out"  | egrep "^Model:" | awk '{print $2}' | xargs)
 cpu_mhz=$(echo "$lscpu_out"  | egrep "CPU MHz:" | awk '{print $3}'| xargs)
-l2_cache=$(echo "$lscpu_out"  | egrep "l2 cache:" | awk '{print $3}'| xargs)
+l2_cache=$(echo "$lscpu_out"  | egrep "^L2 cache:" | awk '{print $3}'| xargs)
 timestamp=$(vmstat -t | tail -1 | awk '{print $18, $19}' | xargs )
 total_mem=$(echo "$vmstat_mb" | tail -1 | awk '{print $4}' | xargs )
 
-
-insert_stmt="INSERT INTO host_info(hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, timestamp, total_mem ) VALUES('$hostname', '$cpu_number', '$cpu_architecture', 'cpu_model', 'cpu_mhz', 'l2_cache', '$timestamp', '$total_mem');"
-
+insert_stmt="INSERT INTO host_info (hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, "timestamp", total_mem)
+VALUES('$hostname', '$cpu_number', '$cpu_architecture', '$cpu_model', '$cpu_mhz', '${l2_cache%%K}', '$timestamp', '$total_mem');"
 export PGPASSWORD=$psql_password
-psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c $insert_stmt
+psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_stmt"
 exit $?
